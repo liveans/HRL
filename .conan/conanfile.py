e19bf8830771321330262b93b1d391b29d1c6230
aaksoy <aaksoy@nettsi.com>
2022-02-26 11:31:26 +0000

    Initial commit

---
+++ .conan/conanfile.py
@@ -0,0 +1,12 @@
+# ______________________________________________________
+# Hot Reload Library conan file
+#
+# @file        conanfile.py
+# @author      Ahmet İbrahim AKSOY <aaksoy@nettsi.com>
+# @date        26.02.2022
+#
+# @copyright   2022 NETTSI Informatics Technology Inc.
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# ______________________________________________________
---
+++ app/src/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * ______________________________________________________
+ * Example application.
+ *
+ * @file         main.cpp
+ * @author       Ahmet İbrahim AKSOY <aaksoy@nettsi.com>
+ * @date         26.02.2022
+ *
+ * @copyright    2022 NETTSI Informatics Technology Inc.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * ______________________________________________________
+ */
+
+#include <iostream>
+
+/**
+ * @brief Main entry point of the application
+ *
+ * @return int exit code
+ */
+auto main() -> int{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
